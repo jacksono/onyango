6d9de0106ad8b46759a2476fccba23180ab51bde
Jackson Onyango <jackson.onyang@andela.com>
2018-08-29 15:48:12 +0300

    Remove un necessary users module

--- server/api.js
+++ server/api.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const users = require('./users');
 const notes = require('./notes');
 const auth = require('./auth');
 
 const api = express.Router();
 api.get('/', (req, res) => res.send({ message: 'Success!' }));
-api.use('/users', users);
 api.use('/notes', notes);
 api.use('/auth', auth);
 
--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Header from './components/header/header';
 import Home from './containers/homePage/home';
-import Users from './containers/usersPage/users';
 import Notes from './containers/notesPage/notes';
 import AddNote from './containers/addNotePage/addNote';
 import ViewNote from './containers/viewNotePage/viewNote';
@@ -18,7 +17,6 @@ const App = () => {
         <div>
           <Header />
           <Route path="/home" component={Home} />
-          <Route path="/users" component={Users} />
           <Route path="/notes" component={Notes} />
           <Route path="/new" component={AddNote} />
           <Route path="/auth/register" component={Register} />
